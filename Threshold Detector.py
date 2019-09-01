ee4d0bf41e4460941943d38cfd1b63130edb4329
Marcus Wong <maclwodafir@gmail.com>
2019-08-31 18:21:44 -0700

    1. Now has a hotcell mask.  To use the hotcell mask: -hot fileNameOfHotCellMask.npz

--- Threshold Detector.py
+++ Threshold Detector.py
@@ -1,43 +1,80 @@
 import rawpy
 import numpy
 import argparse
+import pathlib
+
+
+
+
+def hotcells(array, x_res):
+    
+    hotc = numpy.load(hotcellFile);
+    hotc_array = hotc['hot'];
+    x = hotc_array % (x_res);
+    y = hotc_array // (x_res);
+
+    maskedarray = numpy.ma.masked_array(array);
+    maskedarray[y,x] = numpy.ma.masked;
+    return numpy.ma.filled(maskedarray,0);
 
 def checkPixels(array,size):
-    
-    aboveThreshold = numpy.array(numpy.nonzero(array > threshold));
-    
-    luminosities = array[array > threshold];
-    
-    data = numpy.column_stack((aboveThreshold[1], aboveThreshold[0], luminosities));
-
-    numberOfDetections = len(luminosities);
-
-
-    numpy.savez(fileName,data = data ,numberOfDetections = numberOfDetections);
         
+    aboveThreshold = numpy.array(numpy.nonzero(array > threshold));
+
+    x = aboveThreshold[1];
+    y = aboveThreshold[0];
+    luminosities = array[array > threshold];
+    data = numpy.column_stack((aboveThreshold[1], aboveThreshold[0], luminosities));
+    numberOfDetections = len(luminosities);
     
+    if args.out:
+
+        pathName = pathlib.Path(outputDirectory);
+
+        if not pathName.is_dir():
+            pathName.mkdir(parents = True);
+    
+   
+    numpy.savez(fileName, x = x, y = y, luminosities = luminosities, data = data, numberOfDetections = numberOfDetections);
+
+
+
 def loadImage(name):
-    image = rawpy.imread(name + '.dng');
+    image = rawpy.imread(name);
     array = image.raw_image;
     size = array.shape
+    
+    if args.hot:
+        array = hotcells(array, size[1]);
     
     checkPixels(array,size);
 
 
 parser = argparse.ArgumentParser();
-parser.add_argument('-f', type = str, nargs = '+', help = "Gives Location of Image (.dng Extension Must Not Be Added)");
-parser.add_argument('-o', type = str, nargs = '+', help = "Names of Output .npz Packages", default = 1);
-parser.add_argument('-t', type = int, nargs = 1, help = 'Sets Threshold');
+parser.add_argument('-f', type = str, nargs = '+', help = "gives location of image");
+parser.add_argument('-t', type = int, nargs = 1, help = 'sets threshold', required = True);
+parser.add_argument('-out', type = str, nargs = 1, help = 'sets directory for .npz files to be saved into');
+parser.add_argument('-hot', type = str, nargs = 1, help = 'gives location of npz file for a hot cell mask');
 args = parser.parse_args();
 
 threshold = args.t[0];
 
+if args.hot:
+    hotcellFile = args.hot[0];
 
-for x in range(0,len(args.f)):    
+if args.out:
+    outputDirectory = args.out[0];
+
+if args.f:
+    for files in args.f:    
+        
+        fileName = files.split('.')[0] + '.npz';
+        
+        if args.out:
+            fileName = outputDirectory + '/' + pathlib.PurePath(fileName).name;
+
+        loadImage(files);
     
-    if (args.o == 1 or len(args.o) != len(args.f)):
-        fileName = str(args.f[x] + '.npz');
-    else:
-        fileName = args.o[x];
 
-    loadImage(args.f[x]);
+
+       
